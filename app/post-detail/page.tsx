579f867214c5aae4b75398ce8e07ca83e776ef43
유지수 <jsyu333@gmail.com>
2024-12-20 16:44:33 +0900

    chore: site-header 컴포넌트 상단 추가

--- app/post-detail/page.tsx
+++ app/post-detail/page.tsx
@@ -1,3 +1,4 @@
+import { SiteHeader } from '~/components/common/site-header'
 import { PostArticle } from '~/components/post-article'
 import { PostComment } from '~/components/post-comment'
 import { PostCommentInput } from '~/components/post-comment-input'
@@ -6,22 +7,25 @@ import { Card, CardContent, CardHeader, CardTitle } from '~/components/ui/card'
 // 게시글 뷰페이지
 function PostDetailPage() {
   return (
-    <div className="mx-auto h-screen max-w-[1248px]">
-      <section>
-        <PostArticle />
-      </section>
-      <section className="mt-6">
-        <Card>
-          <CardHeader>
-            <CardTitle>Comments (89)</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <PostComment />
-            <PostComment />
-            <PostCommentInput />
-          </CardContent>
-        </Card>
-      </section>
+    <div className="bg-gray-100">
+      <SiteHeader></SiteHeader>
+      <div className="mx-auto max-w-[1248px] pt-[24px]">
+        <section>
+          <PostArticle />
+        </section>
+        <section className="mt-6">
+          <Card>
+            <CardHeader>
+              <CardTitle>Comments (89)</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <PostComment />
+              <PostComment />
+              <PostCommentInput />
+            </CardContent>
+          </Card>
+        </section>
+      </div>
     </div>
   )
 }
