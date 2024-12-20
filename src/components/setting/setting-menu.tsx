ef4ef20fbed431837ee9c641732b4f7f2a3c1847
E0min <59902527+E0min@users.noreply.github.com>
2024-12-20 16:12:17 +0900

    chore: 네비게이팅 설정

--- src/components/setting/setting-menu.tsx
+++ src/components/setting/setting-menu.tsx
@@ -9,6 +9,7 @@ import {
   FileText,
   LogOut,
 } from 'lucide-react'
+import Link from 'next/link'
 
 function SettingsMenu() {
   return (
@@ -17,9 +18,17 @@ function SettingsMenu() {
       <div>
         <h2 className="mb-2 text-sm text-gray-500">Account</h2>
         <div className="space-y-2">
-          <MenuItem icon={User} label="Personal Information" />
-          <MenuItem icon={Lock} label="Password and Security" />
-          <MenuItem icon={Bell} label="Notifications" />
+          <MenuItem
+            icon={User}
+            label="Personal Information"
+            href="/settings/personal-information"
+          />
+          <MenuItem
+            icon={Lock}
+            label="Password and Security"
+            href="/settings/password-and-security"
+          />
+          <MenuItem icon={Bell} label="Notifications" href="/notify" />
         </div>
       </div>
 
@@ -27,8 +36,12 @@ function SettingsMenu() {
       <div className="mt-6">
         <h2 className="mb-2 text-sm text-gray-500">Other</h2>
         <div className="space-y-2">
-          <MenuItem icon={Info} label="About" />
-          <MenuItem icon={FileText} label="Terms of Service" />
+          <MenuItem icon={Info} label="About" href="/settings/about" />
+          <MenuItem
+            icon={FileText}
+            label="Terms of Service"
+            href="/settings/terms"
+          />
           <MenuItem icon={LogOut} label="Log Out" isLogout />
         </div>
       </div>
@@ -40,21 +53,29 @@ const MenuItem = ({
   icon: Icon,
   label,
   isLogout,
+  href,
 }: {
   icon: React.ComponentType<{ className?: string }>
   label: string
   isLogout?: boolean
+  href?: string // Link의 목적지
 }) => {
+  // 버튼인지 링크인지 결정
+  const Wrapper = href ? Link : 'button'
+
   return (
-    <button
-      className={`flex w-full items-center justify-between rounded-lg px-4 py-2 ${isLogout ? 'text-red-500 hover:bg-red-50' : 'hover:bg-gray-100'}`}
+    <Wrapper
+      href={href}
+      className={`flex w-full items-center justify-between rounded-lg px-4 py-2 ${
+        isLogout ? 'text-red-500 hover:bg-red-50' : 'hover:bg-gray-100'
+      }`}
     >
       <div className="flex items-center space-x-3">
         <Icon className="h-5 w-5 text-gray-600" />
         <span className="text-sm font-medium">{label}</span>
       </div>
       <ChevronRight className="h-5 w-5 text-gray-400" />
-    </button>
+    </Wrapper>
   )
 }
 
