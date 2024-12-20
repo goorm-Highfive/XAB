8997db9e60f890a5dea3ca9343db77004037944a
jyooni <berry3891@naver.com>
2024-12-20 15:52:32 +0900

    feat: notify 페이지  배경색 및 Header 추가

--- app/notify/page.tsx
+++ app/notify/page.tsx
@@ -1,10 +1,14 @@
+import { SiteHeader } from '~/components/common/site-header'
 import { NotifyGroup } from '~/components/notify/notify-group'
 
 function NotifyPage() {
   return (
-    <div className="mb-28 mt-4">
-      <NotifyGroup />
-      <NotifyGroup />
+    <div className="h-screen bg-gray-100">
+      <SiteHeader />
+      <div className="mb-28 mt-4">
+        <NotifyGroup />
+        <NotifyGroup />
+      </div>
     </div>
   )
 }
