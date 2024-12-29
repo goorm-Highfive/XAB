a7abb317ebc4a483fa863f4df40ac31637df9e4d
jyooni <berry3891@naver.com>
2024-12-29 19:22:48 +0900

    feat: signUpPage 기본 UI 구현

---
+++ src/components/sign-up/sign-up-form.tsx
@@ -0,0 +1,53 @@
+import Link from 'next/link'
+
+import { Button } from '~/components/ui/button'
+import { Input } from '~/components/ui/input'
+import { Label } from '~/components/ui/label'
+
+function SignUpForm() {
+  return (
+    <div className="flex flex-col gap-6">
+      <form>
+        <div className="flex flex-col gap-6">
+          <div className="flex flex-col items-center gap-2">
+            <h1 className="text-3xl font-bold">Sign Up for xAB</h1>
+            <div className="text-md text-center">
+              Already have an account?
+              <Link href="#" className="ml-1 underline underline-offset-4">
+                Login
+              </Link>
+            </div>
+          </div>
+          <div className="flex flex-col gap-6">
+            <div className="grid gap-2">
+              <Label htmlFor="userName">Name</Label>
+              <Input id="userName" type="text" required />
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="m@example.com"
+                required
+              />
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="password">Password</Label>
+              <Input id="password" type="password" required />
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="confirmPassword">Confirm Password</Label>
+              <Input id="password" type="confirmPassword" required />
+            </div>
+            <Button type="submit" className="w-full">
+              Sign Up
+            </Button>
+          </div>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export { SignUpForm }
