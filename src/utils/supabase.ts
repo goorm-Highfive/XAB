225d2016090ff7b20d336570093ab666cde18850
E0min <59902527+E0min@users.noreply.github.com>
2024-12-30 18:55:49 +0900

    chore: 필요없는 라이브러리 삭제

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,7 +1,4 @@
 import { createClient, SupabaseClient } from '@supabase/supabase-js'
-import dotenv from 'dotenv'
-
-dotenv.config()
 
 const supabaseUrl: string | undefined = process.env.SUPABASE_URL
 const supabaseKey: string | undefined = process.env.SUPABASE_KEY
