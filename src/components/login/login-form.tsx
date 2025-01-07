055cbc65ffd20da9269876762a2144bbe08c5f85
jyooni <berry3891@naver.com>
2025-01-08 00:45:14 +0900

    feat: 로그인 실패 toast 추가

--- src/components/login/login-form.tsx
+++ src/components/login/login-form.tsx
@@ -1,9 +1,10 @@
 'use client'
-
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { redirect } from 'next/navigation'
+import { toast } from 'sonner'
 
+import { Toaster } from '~/components/ui/sonner'
 import { Button } from '~/components/ui/button'
 import { Form } from '~/components/ui/form'
 import { CustomFormField } from '~/components/common/custom-form-field'
@@ -29,13 +30,15 @@ function LoginForm() {
     })
 
     if (error) {
-      console.error('로그인 실패:', error.message)
-      return
+      console.log(error)
+      toast.error('로그인 실패', {
+        description:
+          '등록되지 않은 아이디거나 아이디 또는 비밀번호를 잘못 입력하였습니다.',
+      })
+    } else {
+      console.log('로그인 성공:', data)
+      redirect('/home')
     }
-
-    console.log('로그인 성공:', data)
-    alert('로그인이 완료되었습니다!')
-    redirect('/home')
   }
 
   return (
@@ -49,6 +52,7 @@ function LoginForm() {
           </Button>
         </div>
       </form>
+      <Toaster richColors />
     </Form>
   )
 }
