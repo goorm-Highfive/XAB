e782619a519b66c0e0393523faf3620fae90e5da
jyooni <berry3891@naver.com>
2025-01-08 22:13:51 +0900

    feat: client에서 인증 상태 관리

---
+++ src/components/auth/client-session-manager.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
+
+import { createClient } from '~/utils/supabase/client'
+
+function ClientSessionManager() {
+  const router = useRouter()
+  const supabase = createClient()
+
+  useEffect(() => {
+    const { data } = supabase.auth.onAuthStateChange((event) => {
+      if (event === 'SIGNED_OUT') {
+        console.log('사용자가 로그아웃 되었습니다.')
+        router.push('/login')
+      }
+    })
+
+    return () => {
+      data.subscription?.unsubscribe()
+    }
+  }, [router])
+
+  return null
+}
+
+export { ClientSessionManager }
