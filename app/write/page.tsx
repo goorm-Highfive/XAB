ac89bb896c385c062e526673a10e024516ecd567
지수 <jsyu333@gmail.com>
2025-01-19 22:45:14 +0900

    fix: error 타입 오류 해결

--- app/write/page.tsx
+++ app/write/page.tsx
@@ -319,15 +319,9 @@ export default function Write() {
       }
 
       router.push('/')
-    } catch (error: unknown) {
+    } catch (error) {
       if (error instanceof Error) {
-        console.error('오류 발생:', error.message)
-        toast.error(
-          error.message || '게시물을 처리하는 중 오류가 발생했습니다.',
-        )
-      } else {
-        console.error('예기치 못한 오류가 발생했습니다:', error)
-        toast.error('예기치 못한 오류가 발생했습니다.')
+        console.error(error.message)
       }
     }
   }
